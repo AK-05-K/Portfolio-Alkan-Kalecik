78d56118bd57f296feabddf5971f95bdae8d7759
Alkan Kalecik <alkan.kalecik@stud.uni-due.de>
2026-03-31 14:25:11 +0200

    Routing zum About me bereich implementiert und about me component erstellt

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { Homepage } from './homepage/homepage';
+import { AboutMe } from './about-me/about-me';
 
 export const routes: Routes = [
-    { path: '', component: Homepage} /* Mit '' wird die component
-    Homepage als Startpunkt festgelegt */
+    { path: '', component: Homepage}, /* Mit '' wird die component Homepage als Startpunkt festgelegt */
+    { path: 'about-me', component: AboutMe }
 ];
--- Angular/src/app/homepage/homepage.ts
+++ Angular/src/app/homepage/homepage.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { TitleBox } from '../title-box/title-box';
 import { VerticalLine } from '../vertical-line/vertical-line';
 import { HorizontalLine } from '../horizontal-line/horizontal-line';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
-  imports: [TitleBox, VerticalLine, HorizontalLine],
+  imports: [TitleBox, VerticalLine, HorizontalLine, RouterModule],
   templateUrl: './homepage.html',
   styleUrl: './homepage.css',
 })
