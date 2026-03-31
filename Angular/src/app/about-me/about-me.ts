227de3b993440b769682c76d91507b627e97d63f
Alkan Kalecik <alkan.kalecik@stud.uni-due.de>
2026-03-31 19:16:48 +0200

    Mit dem About me bereich angefangen. Mobile first ansatz für den ersten Teil fertig, fehlt noch Tablet und Desktop größe

--- Angular/src/app/about-me/about-me.ts
+++ Angular/src/app/about-me/about-me.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { TitleBox } from '../title-box/title-box';
+import { VerticalLine } from '../vertical-line/vertical-line';
+import { HorizontalLine } from '../horizontal-line/horizontal-line';
 
 @Component({
   selector: 'app-about-me',
-  imports: [],
+  imports: [TitleBox, VerticalLine, HorizontalLine],
   templateUrl: './about-me.html',
   styleUrl: './about-me.css',
 })
