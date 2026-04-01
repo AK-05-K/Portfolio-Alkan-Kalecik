4abefeb5761649baaad9835f4e8cc8ef3b23c6b9
Alkan Kalecik <alkan.kalecik@stud.uni-due.de>
2026-04-01 18:52:05 +0200

    vorrübergehenden button um zur homepage zu gelangen

--- Angular/src/app/about-me/about-me.ts
+++ Angular/src/app/about-me/about-me.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { TitleBox } from '../title-box/title-box';
 import { VerticalLine } from '../vertical-line/vertical-line';
 import { HorizontalLine } from '../horizontal-line/horizontal-line';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about-me',
-  imports: [TitleBox, VerticalLine, HorizontalLine],
+  imports: [TitleBox, VerticalLine, HorizontalLine, RouterModule],
   templateUrl: './about-me.html',
   styleUrl: './about-me.css',
 })
