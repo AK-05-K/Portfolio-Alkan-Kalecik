7a277296d730f0b6d219699ec6b0f9118552c625
Alkan Kalecik <alkan.kalecik@stud.uni-due.de>
2026-04-04 17:55:59 +0200

    projects section ohne rückrouting zur homepage und about me für mobile fertig

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Homepage } from './homepage/homepage';
 import { AboutMe } from './about-me/about-me';
+import { Projects } from './projects/projects';
 
 export const routes: Routes = [
     { path: '', component: Homepage}, /* Mit '' wird die component Homepage als Startpunkt festgelegt */
-    { path: 'about-me', component: AboutMe }
+    { path: 'about-me', component: AboutMe },
+    { path: 'projects', component: Projects}
 ];
--- Angular/src/app/projects/projects.ts
+++ Angular/src/app/projects/projects.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { VerticalLine } from '../vertical-line/vertical-line';
+import { HorizontalLine } from '../horizontal-line/horizontal-line';
+import { TitleBox } from '../title-box/title-box';
+
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [VerticalLine, HorizontalLine, TitleBox],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
-export class Projects {}
+export class Projects { }
